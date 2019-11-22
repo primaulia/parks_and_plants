240f182633a7010f029422d64ef8caf5b00f4295
Prima Aulia Gusta <primaulia@gmail.com>
2019-11-22 12:27:02 +0800

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
 # db/seeds.rb
 Tag.destroy_all if Rails.env.development? || Rails.env.test?
+Garden.destroy_all if Rails.env.development?
+
+little = Garden.create!(
+  name: "My Little Garden",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg"
+)
+
+other = Garden.create!(
+  name: "My Other Garden",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
+)
 
 Tag.create(name: 'Flower')
 Tag.create(name: 'Foliage')
